210193300b6f95652c0d137bfad44ed34137c095
robevans <rob@irrelon.com>
2021-03-22 09:17:42 +0000

    Started writing the test skeleton

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,14 @@
+const {Client, Server} = require("../src/index");
+const server = new Server();
+
+const startServer = () => {
+	server.start(10292);
+};
+
+const stopServer = () => {
+	server.stop();
+};
+
 describe("Client", () => {
 	it("Can connect to the server", () => {
 
